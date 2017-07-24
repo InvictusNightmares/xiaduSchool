e5c8b23226b8501f4d4f837e01b31982ea28735b
Invictus- <invictusnightmares@gmail.com>
2017-07-24 15:25:34 +0800

    修改配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" class="img">
     <router-view></router-view>
   </div>
 </template>
@@ -19,5 +19,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.img {
+  background-size: cover;
+  width: 3rem;
+  height: 3rem;
+  margin: 0 auto;
 }
 </style>
